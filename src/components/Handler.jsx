b64fcb6aa3f46230fe1523c762c4831b8841dd08
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-06 21:01:42 +0530

    body section design 1

--- src/components/Handler.jsx
+++ src/components/Handler.jsx
@@ -4,12 +4,12 @@ function Handler() {
   return (
     <>
       <div className='bg-armyBackgroundColor h-screen '>
-        <div className='mx-auto max-w-4xl bg-blue-200'>
+        <div className='mx-auto max-w-4xl bg-blue-200 myContainer'>
           <h1>Force-One-Vault</h1>
           <p>Elite Protection For Your Data</p>
-          <div className='text-white flex flex-col p-4 '>
+          <div className='text-white flex flex-col p-4 gap-2'>
             <input className='rounded-full' type='text' name='' id='' />
-            <div className='flex'>
+            <div className='flex gap-2'>
               <input type='text' name='' id='' />
               <input type='text' name='' id='' />
             </div>

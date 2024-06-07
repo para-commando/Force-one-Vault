dea57ee0bd8834f688b03078fd28bd6c9d19ce38
para-commando <anirudh.nayak.172k@gmail.com>
2024-06-07 22:50:48 +0530

    body section design 6

--- src/components/Handler.jsx
+++ src/components/Handler.jsx
@@ -3,7 +3,7 @@ import tankLogo from '../assets/tank.png';
 import credentialShowIcon from '../assets/show.png';
 import credentialHideIcon from '../assets/hidden.png';
 
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 function Handler() {
   const credentialShowIconImgRef = useRef();
   const [vaultCell, setVaultCell] = useState({
@@ -11,8 +11,30 @@ function Handler() {
     uniqueCredId: '',
     credential: '',
   });
+  const [credsList, setCredsList] = useState([]);
 
-  const saveCredential = () => {};
+  useEffect(() => {
+    let creds = localStorage.getItem('creds');
+    if (creds) {
+      setCredsList(JSON.parse(creds));
+    }
+  }, []);
+
+  const saveCredential = () => {
+    const aaa11 = localStorage.getItem('creds');
+    console.log('🚀 ~ saveCredential ~ aaa11:', aaa11);
+    const creds = {
+      source: vaultCell.source || '',
+      uniqueCredId: vaultCell.uniqueCredId || '',
+      credential: vaultCell.credential || '',
+    };
+
+    setCredsList([...credsList, vaultCell]);
+    localStorage.setItem('creds', JSON.stringify([...credsList, vaultCell]));
+
+    const aaa = localStorage.getItem('creds');
+    console.log('🚀 ~ saveCredential ~ aaa:', aaa);
+  };
 
   const showCredential = () => {
     if (credentialShowIconImgRef.current.src.includes('hidden.png')) {
@@ -22,6 +44,10 @@ function Handler() {
 
     credentialShowIconImgRef.current.src = credentialHideIcon;
     return;
+  };
+
+  const handleInputData = (e) => {
+    setVaultCell({ ...vaultCell, [e.target.name]: e.target.value });
   };
   return (
     <>
@@ -43,27 +69,30 @@ function Handler() {
             <input
               className='rounded-full border-4 border-green-300 text-black text-center w-full'
               value={vaultCell.source}
+              onChange={(e) => handleInputData(e)}
               placeholder='Enter Source URL/Info'
               type='text'
-              name=''
+              name='source'
               id=''
             />
             <div className='flex gap-3 w-full'>
               <input
                 className='rounded-full border-4 border-green-300 text-black text-center w-1/2'
                 value={vaultCell.uniqueCredId}
+                onChange={(e) => handleInputData(e)}
                 placeholder='Unique Credential-id'
                 type='text'
-                name=''
+                name='uniqueCredId'
                 id=''
               />
               <div className='flex items-center relative w-1/2'>
                 <input
                   className='rounded-full border-4 border-green-300 text-black text-center w-full pr-10'
                   value={vaultCell.credential}
+                  onChange={(e) => handleInputData(e)}
                   placeholder='Credential'
+                  name='credential'
                   type='text'
-                  name=''
                   id=''
                 />
                 <span className='absolute right-3' onClick={showCredential}>
@@ -90,6 +119,48 @@ function Handler() {
               Add Credential
             </button>
           </div>
+        </div>
+        <div className='passwords justify-center px-5'>
+          <h2>Your Credentials</h2>
+          <table className='table-auto text-white w-full '>
+            <thead className='border-white border-2 bg-green-800'>
+              <tr className='border-white border-2'>
+                <th className='border-white border-2'>Song</th>
+                <th className='border-white border-2'>Artist</th>
+                <th className='border-white border-2'>Year</th>
+              </tr>
+            </thead>
+            <tbody className='border-white border-2'>
+              <tr className='border-white border-2'>
+                <td className=' text-center border-white border-2'>
+                  The Sliding Mr. Bones (Next Stop, Pottersville)
+                </td>
+                <td className=' text-center border-white border-2'>
+                  {' '}
+                  Malcolm Lockyer
+                </td>
+                <td className=' text-center border-white border-2'>1961</td>
+              </tr>
+              <tr className='border-white border-2'>
+                <td className=' text-center border-white border-2'>
+                  Witchy Woman
+                </td>
+                <td className=' text-center border-white border-2'>
+                  The Eagles
+                </td>
+                <td className=' text-center border-white border-2'>1972</td>
+              </tr>
+              <tr className='border-white border-2'>
+                <td className=' text-center border-white border-2'>
+                  Shining Star
+                </td>
+                <td className=' text-center border-white border-2'>
+                  Earth, Wind, and Fire
+                </td>
+                <td className=' text-center border-white border-2'>1975</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </>
